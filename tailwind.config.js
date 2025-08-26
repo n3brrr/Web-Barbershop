64baeda3c9780e75a22f698b76664042e7b2e919
nebur <neburtl837@gmail.com>
2025-08-26 22:27:54 +0200

    Web base

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#D4AF37',  // Gold
+        'secondary': '#F5F5DC', // Beige
+        'dark': '#000000',     // Black
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
